bafb1f33ff8e8c5f706b0a1d5580875b794cb5a2
Fred Soares <29602236+FredSoares@users.noreply.github.com>
2021-07-05 14:47:28 -0100

    add express environment variables

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 
 const app = express();
+const port = process.env.port || 3000;
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
 mongoose
-    .connect("mongodb://root:myPassword@mongo:27017/?authSource=admin")
+    .connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
     .then(()=> console.log("Successfully connected to DB"))
     .catch((e)=> console.error(e));
 
-const port = process.env.port || 3000;
-
 app.get('/', (req, res) => {
-
     return res.send('Hello World');
 });
 
