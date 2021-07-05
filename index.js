0409406c62fecf17f5862e4c1c8a14e150d8227c
Fred Soares <29602236+FredSoares@users.noreply.github.com>
2021-07-05 15:43:55 -0100

    add depends_on property to docker compose

--- index.js
+++ index.js
@@ -6,14 +6,21 @@ const app = express();
 const port = process.env.port || 3000;
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
-mongoose
-    .connect(mongoURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    })
-    .then(()=> console.log("Successfully connected to DB"))
-    .catch((e)=> console.error(e));
+const connectWithRetry = () => {
+    mongoose
+        .connect(mongoURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
+        .then(()=> console.log("Successfully connected to DB"))
+        .catch((e)=> {
+            console.error(e)
+            setTimeout(connectWithRetry, 5000); // try connect after 5 seconds
+        });    
+}
+
+connectWithRetry();
 
 app.get('/', (req, res) => {
     return res.send('Hello World');
