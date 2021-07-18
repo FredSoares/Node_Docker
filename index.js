3d653c01fae5578dc739b97bda1cf59cbaefabce
Fred Soares <29602236+FredSoares@users.noreply.github.com>
2021-07-17 23:18:01 -0100

    create post crud

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 
+// ROUTES
+const postRouter = require("./routes/postRoutes");
+
 const app = express();
 const port = process.env.port || 3000;
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
@@ -22,10 +25,13 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     return res.send('Hello World');
 });
 
+app.use('/api/v1/posts', postRouter);
 
 app.listen(port, ()=> {
     console.log(`Listen on port ${port}`);
